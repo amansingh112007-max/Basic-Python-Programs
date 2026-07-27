e08e9ad54edb4786b3990531ba858a2d13468e71
Aman Kumar Singh <amantiger112007@gmail.com>
2026-07-27 14:12:36 +0530

    Add files via upload

---
+++ Dynamic swapping.py
@@ -0,0 +1,12 @@
+
+a = input("Enter first number (a): ")
+b = input("Enter second number (b): ")
+print("Before swapping")
+print("a=",a)
+print("b=",b)
+
+a, b = b, a
+
+print("\nAfter swapping:")
+print("a=",a)
+print("b =",b)
